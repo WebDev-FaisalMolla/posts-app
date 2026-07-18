dfceac6d46b2a3d34d406df066c180a4ab2d405d
Faisal Molla <faisalmolla768@gmail.com>
2026-07-18 09:37:59 +0530

    package.json updated for postinstall and some changes at next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: ["192.168.1.16"],
 };
-
-
-module.exports = {
-  allowedDevOrigins: ['192.168.1.16'],
-}
 
 export default nextConfig;
